43f754b0a37fc8732e1182134232066a6246a9ed
Hugo Duncan <hugo@hugoduncan.org>
2012-08-13 17:47:19 -0400

    Update dependencies to use org.cloudhoist release of maven-embedder

--- clj-mvn/project.clj
+++ clj-mvn/project.clj
@@ -1,5 +1,8 @@
 (defproject clj-mvn "0.1.0-SNAPSHOT"
   :description "Maven pom reader for clojure"
-  :dependencies [[com.palletops/maven-embedder "0.1.0-SNAPSHOT"]
+  :dependencies [[org.cloudhoist/maven-embedder "0.1.0"]
                  [org.clojure/clojure "1.3.0"]]
-  :profiles {:dev {:resource-paths ["test-resources"]}})
+  :profiles {:dev {:resource-paths ["test-resources"]}}
+  :repositories
+  {"sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"
+   "sonatype" "https://oss.sonatype.org/content/repositories/releases/"})
