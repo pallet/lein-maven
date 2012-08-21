247f7065164b0d84c263cfca39dcbf88dc9e6072
Hugo Duncan <hugo@hugoduncan.org>
2012-08-21 09:45:45 -0400

    Add metadata to project.clj files

--- clj-mvn/project.clj
+++ clj-mvn/project.clj
@@ -1,8 +1,16 @@
 (defproject clj-mvn "0.1.0-SNAPSHOT"
   :description "Maven pom reader for clojure"
-  :dependencies [[org.cloudhoist/maven-embedder "0.1.0"]
+  :url "https://github.com/pallet/lein-maven"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.cloudhoist/maven-embedder "0.1.1"]
                  [org.clojure/clojure "1.3.0"]]
   :profiles {:dev {:resource-paths ["test-resources"]}}
   :repositories
-  {"sonatype-snapshots" "https://oss.sonatype.org/content/repositories/snapshots"
-   "sonatype" "https://oss.sonatype.org/content/repositories/releases/"})
+  {"sonatype" "https://oss.sonatype.org/content/repositories/releases/"}
+  :scm {:url "git@github.com:pallet/lein-maven.git"}
+  :pom-addition [:developers [:developer
+                              [:name "Hugo Duncan"]
+                              [:url "http://hugoduncan.org"]
+                              [:email "hugo at hugoduncan.org"]
+                              [:timezone "-5"]]])
--- project.clj
+++ project.clj
@@ -1,4 +1,13 @@
 (defproject lein-maven "0.1.0-SNAPSHOT"
   :description "Maven pom reader"
+  :url "https://github.com/pallet/lein-maven"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :profiles {:dev {:dependencies [[leiningen-core "2.0.0-SNAPSHOT"]
-                                  [org.clojure/clojure "1.3.0"]]}})
+                                  [org.clojure/clojure "1.3.0"]]}}
+  :scm {:url "git@github.com:pallet/lein-maven.git"}
+  :pom-addition [:developers [:developer
+                              [:name "Hugo Duncan"]
+                              [:url "http://hugoduncan.org"]
+                              [:email "hugo at hugoduncan.org"]
+                              [:timezone "-5"]]])
